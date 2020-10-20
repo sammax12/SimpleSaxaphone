47a6b3a12556e619711b1ad5934304d89992770a
Alan <alanhp2@hotmail.com>
2020-10-20 11:01:33 -0700

    Added transition to home

--- app/src/main/java/com/example/simplesaxaphone/MainActivity.java
+++ app/src/main/java/com/example/simplesaxaphone/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.simplesaxaphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
     }
 }
 
