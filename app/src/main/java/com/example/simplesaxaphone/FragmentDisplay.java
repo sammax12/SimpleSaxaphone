3f952a6cb5333c523702821522038ca3dcdaf130
Alan <alanhp2@hotmail.com>
2020-10-21 16:19:57 -0700

    implemented a view pager!!!

--- app/src/main/java/com/example/simplesaxaphone/FragmentDisplay.java
+++ app/src/main/java/com/example/simplesaxaphone/FragmentDisplay.java
@@ -3,22 +3,51 @@ package com.example.simplesaxaphone;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FragmentDisplay extends AppCompatActivity {
 
-    FragmentTransaction fragmentTransaction;
+    private SectionsStatePageAdapter mSectionsStatePageAdapter;
+    private ViewPager mViewPager;
+    private Button menubtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_display);
+        //Button
+        menubtn = (Button) findViewById(R.id.menubtn);
+        menubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FragmentDisplay.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-        Test firstFrag = new Test();
-        fragmentTransaction.replace(R.id.fragmentContainer,firstFrag);
-        fragmentTransaction.commit();
+        //Fragment Stuff
+        mSectionsStatePageAdapter= new SectionsStatePageAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.fragmentContainer);
+
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePageAdapter adapter = new SectionsStatePageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Test(),"Test");
+        adapter.addFragment(new test2(), "test2");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber){
+
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 }
--- app/src/main/java/com/example/simplesaxaphone/Test.java
+++ app/src/main/java/com/example/simplesaxaphone/Test.java
@@ -13,12 +13,23 @@ import android.widget.Button;
 
 public class Test extends Fragment {
 
+        private Button contbtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
+        contbtn = (Button) view.findViewById(R.id.contbtn);
+
+        contbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((FragmentDisplay)getActivity()).setViewPager(1);
+            }
+        });
+
+        return view;
     }
 
 }
