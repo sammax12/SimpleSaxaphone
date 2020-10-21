076290817ba196068f37880a5462aef12fd2e87e
Alan <alanhp2@hotmail.com>
2020-10-20 18:30:22 -0700

    added home screen with buttons and lessons

---
+++ app/src/main/java/com/example/simplesaxaphone/Lessons.java
@@ -0,0 +1,14 @@
+package com.example.simplesaxaphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lessons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lessons);
+    }
+}
