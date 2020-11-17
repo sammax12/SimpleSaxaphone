2639a7b972b0fc3aff170e8c0f004ededa3a4564
Alan <alanhp2@hotmail.com>
2020-11-16 16:32:50 -0800

    created chart translation

---
+++ app/src/main/java/com/example/simplesaxaphone/ChartTranslation.java
@@ -0,0 +1,14 @@
+package com.example.simplesaxaphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChartTranslation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_translation);
+    }
+}
