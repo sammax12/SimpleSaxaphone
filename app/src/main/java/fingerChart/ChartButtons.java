d1f75e8ae06453f928e008194cbd6024615954ff
Alan <alanhp2@hotmail.com>
2020-11-18 16:29:13 -0800

    attempted ChartButtons, tried code in FingerChart1

--- app/src/main/java/fingerChart/ChartButtons.java
+++ app/src/main/java/fingerChart/ChartButtons.java
@@ -1,31 +1,25 @@
 package fingerChart;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.simplesaxaphone.FragmentDisplay;
 import com.example.simplesaxaphone.R;
 
-
-public class FingerChart1 extends Fragment implements View.OnClickListener{
-
+public class ChartButtons extends Fragment implements View.OnClickListener{
+    private View view;
     private Button C_btn, Csharp_btn, D_btn, Dsharp_btn, E_btn, F_btn, Fsharp_btn, G_btn, Gsharp_btn, A_btn, Asharp_btn, B_btn, C_high_btn;
     private ImageButton forwardArrow_btn;
 
+    public ChartButtons(View view){
+        this.view = view;
+        createButtons();
+    }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_finger_chart1, container, false);
-
+    public void createButtons() {
         C_btn = (Button) view.findViewById(R.id.C_btn);
         C_btn.setOnClickListener(this);
 
@@ -67,8 +61,6 @@ public class FingerChart1 extends Fragment implements View.OnClickListener{
 
         forwardArrow_btn = (ImageButton) view.findViewById(R.id.forwardArrow_btn);
         forwardArrow_btn.setOnClickListener(this);
-
-        return view;
     }
 
     @Override
